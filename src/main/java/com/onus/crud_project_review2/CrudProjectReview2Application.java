50931d587b3805db03b33d58362424ecf94008de
fiction2st <fiction2st@naver.com>
2025-05-15 19:45:18 +0900

    docker-DB생성

---
+++ src/main/java/com/onus/crud_project_review2/CrudProjectReview2Application.java
@@ -0,0 +1,13 @@
+package com.onus.crud_project_review2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProjectReview2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudProjectReview2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/onus/crud_project_review2/CrudProjectReview2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onus.crud_project_review2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProjectReview2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
